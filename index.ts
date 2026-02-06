4d1849f9cfcc630ad35ff2819257b42612dfc631
leon.ji <leon.ji@mspbots.ai>
2026-02-06 21:51:43 +0800

    remove log

--- index.ts
+++ index.ts
@@ -65,37 +65,7 @@ const plugin = {
         api.registerChannel({ plugin: mspBotsPlugin });
         api.logger.info('MSPBots channel plugin registered. Waiting for POST requests...');
 
-        // MANUAL DEBUG START
-        setTimeout(() => {
-            console.log('[MSPBots] MANUAL DEBUG: Attempting to start account manually...');
-            const mockCtx = {
-                account: {
-                    accountId: 'manual-debug',
-                    token: 'manual-token',
-                    enabled: true,
-                    name: 'Manual Debug'
-                },
-                log: {
-                    info: (msg) => console.log(`[MSPBots Manual] INFO: ${msg}`),
-                    warn: (msg) => console.warn(`[MSPBots Manual] WARN: ${msg}`),
-                    error: (msg) => console.error(`[MSPBots Manual] ERROR: ${msg}`),
-                    debug: (msg) => console.log(`[MSPBots Manual] DEBUG: ${msg}`)
-                },
-                runtime: {
-                    ingest: (msg) => console.log(`[MSPBots Manual] INGEST: ${JSON.stringify(msg)}`)
-                }
-            };
-            
-            try {
-                if (mspBotsPlugin.gateway && mspBotsPlugin.gateway.startAccount) {
-                     mspBotsPlugin.gateway.startAccount(mockCtx);
-                } else {
-                    console.error('[MSPBots] gateway.startAccount is missing!');
-                }
-            } catch (e) {
-                console.error('[MSPBots] Manual start failed:', e);
-            }
-        }, 5000);
+
     },
 };
 
